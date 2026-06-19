d3d707e4baca66bd11ca8b22d37715b58d399f95
Gehna-01 <gehnakhanna2005@gmail.com>
2026-06-19 13:18:46 +0530

    Add EqualArrays class to compare two arrays

---
+++ DSA/GehnaKhanna_590013140/Day_5/Question2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class EqualArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of arrays: ");
+        int n = sc.nextInt();
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        boolean equal = true;
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
+                equal = false;
+                break;
+            }
+        }
+
+        System.out.println(equal);
+        
+        sc.close();
+    }
+}
